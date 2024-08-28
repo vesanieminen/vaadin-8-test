45deac51c8efc6b2579f5992892ae75c13b0166b
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-08-28 14:36:38 +0300

    Refactoring.

--- src/main/java/org/test/MenuBarTestParent.java
+++ src/main/java/org/test/MenuBarTestParent.java
@@ -2,7 +2,7 @@ package org.test;
 
 import com.vaadin.ui.*;
 
-public abstract class MenuBarTestParent extends MyView {
+public abstract class MenuBarTestParent extends MyAbstractView {
 
     public abstract MenuBar.Command test(Label label, MenuBar menuBar);
 
--- src/main/java/org/test/MyAbstractView.java
+++ src/main/java/org/test/MyAbstractView.java
@@ -3,7 +3,7 @@ package org.test;
 import com.vaadin.server.Page;
 import com.vaadin.ui.CssLayout;
 
-public abstract class MyView extends CssLayout {
+public abstract class MyAbstractView extends CssLayout {
 
     public void browserEvent(Page.BrowserWindowResizeEvent browserWindowResizeEvent) {
     }
