8126c2946ee447f5c884cc13c3a476d516fd8b8a
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-08-28 14:59:39 +0300

    Refactoring.

--- src/main/java/org/test/MenuBarTestClass.java
+++ src/main/java/org/test/MenuBarTestClass.java
@@ -3,7 +3,7 @@ package org.test;
 import com.vaadin.ui.*;
 import org.vaadin.viritin.layouts.MHorizontalLayout;
 
-public class MenuBarTestParentClass extends MenuBarTestParent {
+public class MenuBarTestClass extends MenuBarTestParent {
     @Override
     public MenuBar.Command test(Label label, MHorizontalLayout mHorizontalLayout, MenuBar menuBar) {
         return null;
