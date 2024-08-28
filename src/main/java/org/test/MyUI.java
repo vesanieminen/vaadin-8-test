29fc9d38c2e86c69eb3d16baf85778f2ea2f1121
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-08-28 14:59:17 +0300

    Cleaned imports.

--- src/main/java/org/test/MyUI.java
+++ src/main/java/org/test/MyUI.java
@@ -1,11 +1,9 @@
 package org.test;
 
 import com.vaadin.annotations.Theme;
-import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
