8fc82ca6ebbffa9bf5531b9747d741feecd463d7
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-08-28 14:49:22 +0300

    Added viritin into the mix.

--- src/main/java/org/test/MenuBarTestParent.java
+++ src/main/java/org/test/MenuBarTestParent.java
@@ -1,9 +1,10 @@
 package org.test;
 
 import com.vaadin.ui.*;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
 
 public abstract class MenuBarTestParent extends MyAbstractView {
 
-    public abstract MenuBar.Command test(Label label, MenuBar menuBar);
+    public abstract MenuBar.Command test(Label label, MHorizontalLayout mHorizontalLayout, MenuBar menuBar);
 
 }
--- src/main/java/org/test/MenuBarTestParentClass.java
+++ src/main/java/org/test/MenuBarTestParentClass.java
@@ -1,10 +1,11 @@
 package org.test;
 
 import com.vaadin.ui.*;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
 
 public class MenuBarTestParentClass extends MenuBarTestParent {
     @Override
-    public MenuBar.Command test(Label label, MenuBar menuBar) {
+    public MenuBar.Command test(Label label, MHorizontalLayout mHorizontalLayout, MenuBar menuBar) {
         return null;
     }
 
