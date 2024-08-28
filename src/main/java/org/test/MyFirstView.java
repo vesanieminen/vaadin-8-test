c6cbcdd549129eb2bbdf0ae576e4d0fa4537ee31
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-08-28 15:17:42 +0300

    Added setStyleName call.

--- src/main/java/org/test/MyFirstView.java
+++ src/main/java/org/test/MyFirstView.java
@@ -3,6 +3,11 @@ package org.test;
 import com.vaadin.ui.*;
 
 public class MyFirstView extends MyParentView {
+
+    public MyFirstView() {
+        setStyleName("MyFirstView");
+    }
+
     @Override
     public MenuBar.Command test(Label label, MenuBar menuBar) {
         label.setValue("first");
