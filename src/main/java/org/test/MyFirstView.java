15e57ee887cb556e084620a25befe57743eae7b5
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-08-28 15:25:54 +0300

    Added GridLayout.setComponentAlignment issue.

--- src/main/java/org/test/MyFirstView.java
+++ src/main/java/org/test/MyFirstView.java
@@ -9,6 +9,9 @@ public class MyFirstView extends MyParentView {
         // The following line results in this after migration:
         // ((HasStyle) setClassName()).setStyleName("MyFirstView");
         setStyleName("MyFirstView");
+
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.setComponentAlignment(new Label("label"), Alignment.BOTTOM_CENTER);
     }
 
     @Override
